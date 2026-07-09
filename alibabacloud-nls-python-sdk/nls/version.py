6dd427368c1a5443887907965d2905a0cbed38b2
luke23334 <17092019+luke23334@user.noreply.gitee.com>
2026-07-09 17:30:34 +0800

    chore: 清理个人文件，补充 .gitignore 规则

--- alibabacloud-nls-python-sdk/nls/version.py
+++
@@ -1,2 +0,0 @@
-# Copyright (c) Alibaba, Inc. and its affiliates.
-__version__ = '1.0.0'
--- alibabacloud-nls-python-sdk/tests/test_token.py
+++
@@ -1,7 +0,0 @@
-from nls.token import getToken
-
-from tests.test_utils import TEST_ACCESS_AKID, TEST_ACCESS_AKKEY
-
-
-info = getToken(TEST_ACCESS_AKID, TEST_ACCESS_AKKEY)
-print(info)
